500f4312325f6345de803d83fffc3c0d4eaec038
utsugiriso <utsugiriso@gmail.com>
2018-07-02 02:28:00 +0900

    first commit

---
+++ PostLogToChatworkForWindows/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace PostLogToChatworkForWindows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBoxLogFilePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxFilter = new System.Windows.Forms.TextBox();
+            this.textBoxApiToken = new System.Windows.Forms.TextBox();
+            this.textBoxRoomId = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBoxLogFilePath
+            // 
+            this.textBoxLogFilePath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxLogFilePath.Location = new System.Drawing.Point(94, 12);
+            this.textBoxLogFilePath.Name = "textBoxLogFilePath";
+            this.textBoxLogFilePath.Size = new System.Drawing.Size(437, 19);
+            this.textBoxLogFilePath.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ログファイルパス";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "フィルター";
+            // 
+            // textBoxFilter
+            // 
+            this.textBoxFilter.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxFilter.Location = new System.Drawing.Point(94, 37);
+            this.textBoxFilter.Name = "textBoxFilter";
+            this.textBoxFilter.Size = new System.Drawing.Size(518, 19);
+            this.textBoxFilter.TabIndex = 3;
+            // 
+            // textBoxApiToken
+            // 
+            this.textBoxApiToken.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxApiToken.Location = new System.Drawing.Point(94, 62);
+            this.textBoxApiToken.Name = "textBoxApiToken";
+            this.textBoxApiToken.Size = new System.Drawing.Size(518, 19);
+            this.textBoxApiToken.TabIndex = 4;
+            // 
+            // textBoxRoomId
+            // 
+            this.textBoxRoomId.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxRoomId.Location = new System.Drawing.Point(94, 87);
+            this.textBoxRoomId.Name = "textBoxRoomId";
+            this.textBoxRoomId.Size = new System.Drawing.Size(518, 19);
+            this.textBoxRoomId.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "APIトークン";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "ルームID";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(537, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "ブラウズ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(12, 112);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(600, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "ウォッチを開始する";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 142);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxRoomId);
+            this.Controls.Add(this.textBoxApiToken);
+            this.Controls.Add(this.textBoxFilter);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxLogFilePath);
+            this.Name = "Form1";
+            this.Text = "PostLogToChatworkForWindows";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBoxLogFilePath;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxFilter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxRoomId;
+        private System.Windows.Forms.TextBox textBoxApiToken;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ PostLogToChatworkForWindows/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Net.Http;
+
+namespace PostLogToChatworkForWindows
+{
+    public partial class Form1 : Form
+    {
+        private delegate void DelegateRead(string message);
+        private bool watching = false;
+        Process powerShellProcess;
+        private static HttpClient client = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            client = new HttpClient();
+
+            textBoxLogFilePath.Text = Properties.Settings.Default.logFilePath;
+            textBoxFilter.Text = Properties.Settings.Default.filter;
+            textBoxApiToken.Text = Properties.Settings.Default.apiToken;
+            textBoxRoomId.Text = Properties.Settings.Default.roomId;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBoxLogFilePath.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void button2_Click(object sender2, EventArgs e2)
+        {
+            if (watching)
+            {
+                watching = false;
+                button2.Text = "ウォッチを開始";
+
+                textBoxLogFilePath.ReadOnly = false;
+                textBoxApiToken.ReadOnly = false;
+                textBoxRoomId.ReadOnly = false;
+
+                if (powerShellProcess != null & !powerShellProcess.HasExited)
+                    powerShellProcess.CloseMainWindow();
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(textBoxLogFilePath.Text))
+                {
+                    MessageBox.Show("ログファイルパスを入力してください", "入力漏れ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+                if (string.IsNullOrEmpty(textBoxApiToken.Text))
+                {
+                    MessageBox.Show("APIトークンを入力してください", "入力漏れ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+                if (string.IsNullOrEmpty(textBoxRoomId.Text))
+                {
+                    MessageBox.Show("ルームIDを入力してください", "入力漏れ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+                if (!File.Exists(textBoxLogFilePath.Text))
+                {
+                    MessageBox.Show("ログファイルが存在しません", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                watching = true;
+                button2.Text = "ウォッチを停止";
+
+                textBoxLogFilePath.ReadOnly = true;
+                textBoxApiToken.ReadOnly = true;
+                textBoxRoomId.ReadOnly = true;
+
+                client.DefaultRequestHeaders.Remove("X-ChatWorkToken");
+                client.DefaultRequestHeaders.Add("X-ChatWorkToken", textBoxApiToken.Text);
+
+                powerShellProcess = new Process();
+                powerShellProcess.StartInfo.FileName = "powershell.exe";
+                powerShellProcess.StartInfo.UseShellExecute = false;
+                powerShellProcess.StartInfo.Arguments = $"& Get-Content -Path '{textBoxLogFilePath.Text}' -Tail 0 -Wait";
+                powerShellProcess.StartInfo.RedirectStandardOutput = true;
+                powerShellProcess.OutputDataReceived += new DataReceivedEventHandler((sender, e) =>
+                {
+                    try
+                    {
+                        if (!string.IsNullOrEmpty(e.Data) && (string.IsNullOrEmpty(textBoxFilter.Text) || Regex.IsMatch(e.Data, textBoxFilter.Text)))
+                        {
+                            string requestUri = $"https://api.chatwork.com/v2/rooms/{textBoxRoomId.Text}/messages";
+                            client.PostAsync(requestUri, new FormUrlEncodedContent(new Dictionary<string, string> { { "body", e.Data } }));
+                        }
+                    }
+                    catch (Exception exception)
+                    {
+                        MessageBox.Show(exception.Message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                });
+                powerShellProcess.Start();
+                powerShellProcess.BeginOutputReadLine();
+            }
+
+        }
+
+        private void SaveSettings()
+        {
+            Properties.Settings.Default.logFilePath = textBoxLogFilePath.Text;
+            Properties.Settings.Default.filter = textBoxFilter.Text;
+            Properties.Settings.Default.apiToken = textBoxApiToken.Text;
+            Properties.Settings.Default.roomId = textBoxRoomId.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveSettings();
+            if (powerShellProcess != null && !powerShellProcess.HasExited)
+                powerShellProcess.CloseMainWindow();
+        }
+    }
+}
